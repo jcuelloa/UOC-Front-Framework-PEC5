855d7bbc1cd8508cfbfb055771728ceb06d18cd3
Juan Antonio Cuello Alarcon <jcuelloa@uoc.edu>
2023-05-26 15:36:13 +0200

    formulario OK sin estilos

--- PEC5_Ej_Prac/ecommerce/src/app/article-new-template/article-new-template.component.ts
+++ PEC5_Ej_Prac/ecommerce/src/app/article-new-template/article-new-template.component.ts
@@ -13,18 +13,28 @@ export class ArticleNewTemplateComponent {
   public novalido:boolean = false;
   public message2 = '';
   public nuevoArticulo!:Article;
+  public aLaVenta:boolean=true; //para activar el checkbox por defecto
   
-  constructor() { }
+  
+  constructor() {  }
 
   crearArticulo(articuloForm:NgForm)  {
     if (articuloForm.invalid) {
       //this.message = 'Please correct all errors and resubmit the form';
       this.novalido = true;
     } else {
-      const articulo: Article = articuloForm.value.articulo;
-      console.log('Creating product', articulo);
+      //const articulo: Article = articuloForm.value.articulo;
+      this.nuevoArticulo = articuloForm.value.articulo;
+      
+      //El checkbox toma por defecto in valor vacio. Lo pongo a false
+      if (!this.nuevoArticulo.isOnSale){
+        this.nuevoArticulo.isOnSale=false;
+      }
+      
+      console.log('Crando un nuevo articulo-->', this.nuevoArticulo);
+      
       //this.message2 = "NUEVO ARTICULO-->"+JSON.stringify(articulo);
-      this.nuevoArticulo = articulo;
+      //this.nuevoArticulo = articulo;
     }
   }
 
